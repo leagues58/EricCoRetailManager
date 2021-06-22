6a62ae5ed0e4be378b29aacc4e362efb61ebeac4
Eric Reeves <leagues58@gmail.com>
2021-06-22 18:58:30 -0400

    Added login view

--- ERMDesktopUI/ViewModels/ShellViewModel.cs
+++ ERMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace ERMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginViewModel;
+
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginViewModel = loginVM;
+            ActivateItem(_loginViewModel);
+        }
     }
 }
