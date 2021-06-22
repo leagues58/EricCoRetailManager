469dde0f7d3ade4ad9f7da5e8a8536f7652cc4ed
Eric Reeves <leagues58@gmail.com>
2021-06-21 23:19:39 -0400

    Added Simple Container DI system

--- ERMDesktopUI/Bootstrapper.cs
+++ ERMDesktopUI/Bootstrapper.cs
@@ -18,6 +18,21 @@ namespace ERMDesktopUI
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
