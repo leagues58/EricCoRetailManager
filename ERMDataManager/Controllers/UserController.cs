a56143acf0cc0ce2d8ef93f572a94b067ea06ada
Eric Reeves <leagues58@gmail.com>
2021-06-23 22:29:57 -0400

    Added data access library & wired up to new User controller in API

---
+++ ERMDataManager/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using ERMDataManager.Library.DataAccess;
+using ERMDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ERMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<User> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
